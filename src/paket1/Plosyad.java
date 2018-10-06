4ca0a0675533134bec21fefb9feb58a10808e31d
IonovGeorgiy <43678598+IonovGeorgiy@users.noreply.github.com>
2018-10-06 19:20:18 +0300

    Add files via upload

---
+++ src/paket1/Plosyad.java
@@ -0,0 +1,8 @@
+package paket1;
+public class Plosyad  {
+    int S;                                                                  
+    public void square (int length_diagonals1, int length_diagonals2 ) {
+        S = (length_diagonals1 * length_diagonals2) / 2;
+        System.out.println("Square: " + S);    
+    }
+}
---
+++ src/paket2/My_Class.java
@@ -0,0 +1,9 @@
+package paket2;
+import paket1.*;
+public class My_Class extends Plosyad {
+   public static void main(String args[]) {
+      int length_diagonals1 = 10, length_diagonals2 = 20;
+      My_Class cal = new My_Class();
+      cal.square(length_diagonals1,length_diagonals2);
+   }
+}
